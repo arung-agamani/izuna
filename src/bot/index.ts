19d36177870a23fc7dae1b1594f61b4726621332
Arung Agamani <arung.agamani@gmail.com>
2022-07-14 16:16:04 +0800

    fix: spawn new docker container instance instead

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,7 +3,7 @@ import { SapphireClient } from "@sapphire/framework";
 function createBotApp() {
     const client = new SapphireClient({
         intents: ["GUILDS", "GUILD_MESSAGES"],
-        regexPrefix: new RegExp("^sugar[,! ]", "i"),
+        regexPrefix: new RegExp("^closure[,! ]", "i"),
     });
     client.login(process.env["DISCORD_BOT_TOKEN"]);
     return client;
