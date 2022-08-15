e0f30f5d5d25cf8f2b921540d8667cc3072f05fd
Arung Agamani <arung.agamani@gmail.com>
2022-08-15 16:28:52 +0800

    feat: change on should command to accomodate new weighting

--- src/bot/commands/should.ts
+++ src/bot/commands/should.ts
@@ -1,5 +1,7 @@
+import prisma from "../../lib/prisma";
 import { Command } from "@sapphire/framework";
 import type { Message } from "discord.js";
+import { sub } from "date-fns";
 
 export class ShouldCommand extends Command {
     public constructor(context: Command.Context, options: Command.Options) {
@@ -22,6 +24,52 @@ export class ShouldCommand extends Command {
     }
 
     public async messageRun(message: Message) {
+        const userScore = await prisma.feedRecord.aggregate({
+            _sum: {
+                amount: true,
+            },
+            where: {
+                from: message.author.id,
+            },
+        });
+        const totalScore = await prisma.feedRecord.aggregate({
+            _sum: {
+                amount: true,
+            },
+        });
+        const activationFunction = (user: number, total: number) => {
+            const ratio = Math.exp(Math.log(user / total));
+            return ratio;
+        };
+        const positiveAnswers = ["Yes", "Yes!", "YESSS!!!"];
+        const ssrAnswer =
+            "Of course yes, master. Anything you think the best I also think the best";
+        const whiskasRecently = await prisma.feedRecord.findFirst({
+            where: {
+                amount: 10,
+                date: {
+                    gt: sub(new Date(), { hours: 3 }),
+                },
+            },
+        });
+        if (
+            activationFunction(
+                userScore._sum.amount!,
+                totalScore._sum.amount!
+            ) > 0.8
+        ) {
+            if (whiskasRecently) {
+                await message.channel.send(ssrAnswer);
+                return;
+            }
+            const answer =
+                positiveAnswers[
+                    Math.floor(Math.random() * positiveAnswers.length)
+                ]!;
+            await message.channel.send(answer);
+            return;
+        }
+
         const answers = [
             "Yes",
             "No",
