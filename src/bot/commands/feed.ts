7fd0e462cf74ce738dddb044c46a01fc9617eba3
Arung Agamani <arung.agamani@gmail.com>
2022-08-17 16:24:39 +0800

    style: rework foods for better readability

--- src/bot/commands/feed.ts
+++ src/bot/commands/feed.ts
@@ -3,35 +3,66 @@ import type { Message } from "discord.js";
 import prisma from "../../lib/prisma";
 import { sub, formatDistanceToNow } from "date-fns";
 
+interface Response {
+    name: string;
+    weight: number;
+    response: string;
+}
+
 export class FeedCommand extends Command {
     public constructor(context: Command.Context, options: Command.Options) {
         super(context, {
             ...options,
             name: "feed",
             aliases: ["makan"],
-            description: "Feeds Sugar donut or roti yoland or whiskas.",
+            description: "Feeds Sugar various stuff.\nAlso gacha hell, haha",
         });
     }
 
     public async messageRun(message: Message) {
-        const food = [
-            "Donut",
-            "Roti Yoland",
-            "Black Thunder",
-            "Burger Nasi GKUB",
-            "Nasi Ayam Mang Otot",
-            "Ayam Crisbar",
-            "Whiskas",
-        ];
-        const foodWeight = [1, 2, 3, 5, 7, 8, 10];
-        const foodResponses = [
-            "...\n..\n.\nAren't ya one cheap person. But still appreciated, ...n-nyaa.",
-            "Now we're talking! Hey, wanna know what flavor of roti yoland that I think the best? Hehe.... ひ。み。つ！",
-            "Wow, this is something. Sweet stuff! And crunchy! (❁´◡`❁)",
-            "Hmm... Something pricey... And this small? Alright, not really my favorite because it can get messy real fast, but I do like the flavor!\n",
-            "Ah I remember this. Not only the fact you can do paylater mechanic with mang otot, they have various of sauces too!\nCan you guess what are my favorite combos of sauce???",
-            "MmmhhhHH!!!!!! Gotta love how you can wareg as much as you want on these Crisbar stalls.\nHey, hey, I can handle up to 5!.... Level 5 of spiciness, I mean!\nI didn't imply something else, alright!",
-            "WOOAAAAAAAHHH! ARE YOU SURE YOU'RE GIVING ME THIS, NYAA? MMMHHHHHH CAN I CALL YOU MASTER FROM NOW ON?? CAN I??!! CAN I???!!!!!!!",
+        const foods: Response[] = [
+            {
+                name: "Donut",
+                weight: 1,
+                response:
+                    "...\n..\n.\nAren't ya one cheap person. But still appreciated, ...n-nyaa.",
+            },
+            {
+                name: "Roti Yoland",
+                weight: 2,
+                response:
+                    "Now we're talking! Hey, wanna know what flavor of roti yoland that I think the best? Hehe.... ひ。み。つ！",
+            },
+            {
+                name: "Black Thunder",
+                weight: 3,
+                response:
+                    "Wow, this is something. Sweet stuff! And crunchy! (❁´◡`❁)",
+            },
+            {
+                name: "Burger Nasi GKUB",
+                weight: 5,
+                response:
+                    "Hmm... Something pricey... And this small? Alright, not really my favorite because it can get messy real fast, but I do like the flavor!\n",
+            },
+            {
+                name: "Nasi Ayam Mang Otot",
+                weight: 7,
+                response:
+                    "Ah I remember this. Not only the fact you can do paylater mechanic with mang otot, they have various of sauces too!\nCan you guess what are my favorite combos of sauce???",
+            },
+            {
+                name: "Ayam Crisbar",
+                weight: 8,
+                response:
+                    "MmmhhhHH!!!!!! Gotta love how you can wareg as much as you want on these Crisbar stalls.\nHey, hey, I can handle up to 5!.... Level 5 of spiciness, I mean!\nI didn't imply something else, alright!",
+            },
+            {
+                name: "Whiskas",
+                weight: 10,
+                response:
+                    "WOOAAAAAAAHHH! ARE YOU SURE YOU'RE GIVING ME THIS, NYAA? MMMHHHHHH CAN I CALL YOU MASTER FROM NOW ON?? CAN I??!! CAN I???!!!!!!!",
+            },
         ];
         const gachaFunction = () => {
             const rng = Math.random();
@@ -47,17 +78,17 @@ export class FeedCommand extends Command {
                 const idx = [3, 4];
                 return idx[Math.floor(Math.random() * idx.length)]!;
             } else if (rng > 0.03) {
-                // SSR
+                // Sangat Super Rare
                 const idx = [5];
                 return idx[Math.floor(Math.random() * idx.length)]!;
             } else {
-                // UR
+                // UWOOOOOGH Rare
                 return 6;
             }
         };
         const rngIndex = gachaFunction() || 0;
-        const rngChoice = food[rngIndex];
-        const rngWeight = foodWeight[rngIndex];
+        const rngChoice = foods[rngIndex]?.name;
+        const rngWeight = foods[rngIndex]?.weight;
         let user = await prisma.user.findUnique({
             where: {
                 uid: message.author.id,
@@ -94,7 +125,7 @@ export class FeedCommand extends Command {
             },
         });
         await message.channel.send(
-            `[BETA]\nYou've fed me ${rngChoice}!\n${foodResponses[rngIndex]}`
+            `[BETA]\nYou've fed me ${rngChoice}!\n${foods[rngIndex]?.response}`
         );
     }
 }
