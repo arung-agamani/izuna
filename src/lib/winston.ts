2bd5f4664d13e9bab4d590a8e66645748b1dbfa6
Arung Agamani <arung.agamani@gmail.com>
2023-06-01 21:00:32 +0800

    feat: add logging to loki

--- src/lib/winston.ts
+++ src/lib/winston.ts
@@ -1,4 +1,5 @@
 import winston, { format } from "winston";
+import LokiTransport from "winston-loki";
 const colors = {
     error: "red",
     warn: "yellow",
@@ -22,6 +23,15 @@ const logger = winston.createLogger({
                 format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
                 format.printf((info) => `${info["timestamp"]} [${info.level}] - ${info.message}`)
             ),
+        }),
+        new LokiTransport({
+            host: process.env["NODE_ENV"] === "development" ? "http://localhost:3100" : "http://loki:3100",
+            batching: false,
+            format: winston.format.combine(format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }), format.label({ label: "closure" }), format.json()),
+            level: "debug",
+            labels: {
+                app: "closure",
+            },
         }),
     ],
 });
